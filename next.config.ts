6ea356d932f28d8f93a88f1f8b7769b59970d228
Harsh Srivastava <nott.harsh30@gmail.com>
2025-11-28 10:28:08 +0000

    first deploy

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  eslint: {
+    ignoreDuringBuilds: true, // Prevent ESLint errors from blocking production builds
+  },
 };
 
 export default nextConfig;
